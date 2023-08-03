10c93deb442b3b7d2010bde31edd62ff2370c195
Jayden Ju (주재훈) <121030294+jaehunju1996@users.noreply.github.com>
2023-08-03 16:41:58 +0900

    23.08.03
    study

--- greet.php
+++ greet.php
@@ -1,4 +1,5 @@
 <?php include "sub-header.php"?>
       <h2>인사말</h2>
       본문...
+      <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 <?php include "sub-footer.php"?>
--- js/gnb.js
+++ js/gnb.js
@@ -1,4 +1,30 @@
 $(function () {
+  var rafId;
+  var isMotionStart = true;
+  var fnHeaderMotion = function () {
+    if (!isMotionStart) return;
+    isMotionStart = false;
+    rafId = requestAnimationFrame(function () {
+      var scry = $(window).scrollTop();
+      if (scry >= 40) {
+        $('header').addClass('active');
+      } else {
+        $('header').removeClass('active');
+      }
+      isMotionStart = true;
+    });
+  }; //fnHeaderMotion
+
+  fnHeaderMotion();
+
+  $(window)
+    .scroll(function () {
+      fnHeaderMotion();
+    })
+    .resize(function () {
+      fnHeaderMotion();
+    });
+
   $('.gnb > ul > li').mouseenter(function () {
     $(this).children('ul').slideDown(200);
   }); //mouseenter
